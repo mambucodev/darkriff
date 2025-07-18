72b42f88c18640b38cdb02c6177b296cfc2f4ed9
Mambuco <gabriele.giambrone@icloud.com>
2025-07-18 18:02:34 +0200

    saturated colors, added buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 export default function Home() {
   return (
     <div className="container-fill">
-      <div className="box">        
-        <h1>Welcome to DarkRiff</h1>
-        <h2>Skeumorphism</h2>
+      <div className="box">
+        <div className="box-content">
+          <h1>Welcome to DarkRiff</h1>
+          <h2>Skeumorphism</h2>
+        </div>
+        <div className="navbar">
+          <button>A</button>
+          <button>B</button>
+          <button>C</button>
+          <button>D</button>
+          <button>E</button>
+        </div>
       </div>
     </div>
   );
