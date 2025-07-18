b64abcd3909d52c67fea9acbb109b8905ec8012a
Mambuco <gabriele.giambrone@icloud.com>
2025-07-18 23:56:04 +0200

    completed buttons and cover

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import type { Metadata } from "next";
+import { Space_Grotesk } from "next/font/google";
+import type { Metadata, Viewport } from "next";
 import "@/scss/style.scss";
 
 export const metadata: Metadata = {
@@ -6,13 +7,23 @@ export const metadata: Metadata = {
   description: "A web radio station for rock and metal music lovers!",
 };
 
+export const viewport: Viewport = {
+  themeColor: "hsl(36, 70%, 85%)",
+}
+
+const space_grotesk = Space_Grotesk({
+  subsets: ["latin"],
+  fallback: ['system-ui', 'arial'],
+  variable: '--font-space-grotesk',
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={space_grotesk.variable}>
       <body>
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,35 @@
+import { PauseIcon, PlayIcon, RewindIcon, FastForwardIcon, DotsThreeOutlineVerticalIcon } from "@phosphor-icons/react/ssr";
+// import { SiSoundcloud } from "@icons-pack/react-simple-icons";
+
 export default function Home() {
   return (
     <div className="container-fill">
       <div className="box">
         <div className="box-content">
-          <h1>Welcome to DarkRiff</h1>
-          <h2>Skeumorphism</h2>
+          <div className="cover">
+            <img src="/cover.jpg" />
+            <div className="details">
+              <strong>YOUtopia</strong> - Bring Me The Horizon
+            </div>
+          </div>
         </div>
+
         <div className="navbar">
-          <button>A</button>
-          <button>B</button>
-          <button>C</button>
-          <button>D</button>
-          <button>E</button>
+          <button>
+            <RewindIcon weight="fill" />
+          </button>
+          <button className="active">
+            <PlayIcon weight="fill" />
+          </button>
+          <button>
+            <PauseIcon weight="fill" />
+          </button>
+          <button>
+            <FastForwardIcon weight="fill" />
+          </button>
+          <button>
+            <DotsThreeOutlineVerticalIcon weight="fill" />
+          </button>
         </div>
       </div>
     </div>
