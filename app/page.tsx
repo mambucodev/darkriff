a019045c586a5da42f5b2babd80428c30f49d8be
Mambuco <gabriele.giambrone@icloud.com>
2025-07-19 17:25:22 +0200

    added song progress with slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+"use client"
+
 import { PauseIcon, PlayIcon, RewindIcon, FastForwardIcon, DotsThreeOutlineVerticalIcon } from "@phosphor-icons/react/ssr";
 // import { SiSoundcloud } from "@icons-pack/react-simple-icons";
+import { ChangeEvent, useState } from "react";
 
 export default function Home() {
+  const [songProgress, setSongProgress] = useState<number>(0);
+  const [status, setStatus] = useState<boolean>(false);
+  const [showSettings, setShowSettings] = useState<boolean>(false);
+
+  function parseDuration(secondsRaw: number) {
+    const minutes = Math.floor(secondsRaw / 60);
+    const seconds = secondsRaw % 60;
+    
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  }
+  // testing
+  const songDuration = 242;
+
   return (
     <div className="container-fill">
       <div className="box">
@@ -12,22 +28,27 @@ export default function Home() {
               <strong>YOUtopia</strong> - Bring Me The Horizon
             </div>
           </div>
+          <div className="slider">
+            <span>{parseDuration(songProgress)}</span>
+            <input type="range" min={0} value={songProgress} onChange={(event) => setSongProgress(parseInt(event.target.value))} max={songDuration} />
+            <span>-{parseDuration(songDuration - songProgress)}</span>
+          </div>
         </div>
 
         <div className="navbar">
           <button>
             <RewindIcon weight="fill" />
           </button>
-          <button className="active">
+          <button onMouseDown={() => setStatus(true)} className={status ? "active" : ""}>
             <PlayIcon weight="fill" />
           </button>
-          <button>
+          <button onMouseDown={() => setStatus(false)} className={status ? "" : "active"}>
             <PauseIcon weight="fill" />
           </button>
           <button>
             <FastForwardIcon weight="fill" />
           </button>
-          <button>
+          <button onMouseDown={() => setShowSettings(!showSettings)} className={showSettings ? "active" : ""}>
             <DotsThreeOutlineVerticalIcon weight="fill" />
           </button>
         </div>
