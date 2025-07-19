e1bed468ff7baa12dc667b3f8a5a530d36b00c29
Mambuco <gabriele.giambrone@icloud.com>
2025-07-20 00:51:23 +0200

    add footnote

--- app/page.tsx
+++ app/page.tsx
@@ -26,39 +26,46 @@ export default function Home() {
   return (
     <div className="container-fill">
       <h1>Darkriff Station</h1>
-      <div className="box">
-        <div className="box-content">
-          <div className="cover">
-            <img src="/cover.jpg" loading="eager" />
-            <div className="details">
-              <strong>YOUtopia</strong> - Bring Me The Horizon
+
+      <div className="box-grouping">
+        <div className="box">
+          <div className="box-content">
+            <div className="cover">
+              <div style={{ backgroundImage: `url("${cover}")` }} className="cover-image"></div>
+              <div className="details">
+                <strong>YOUtopia</strong> - Bring Me The Horizon
+              </div>
+            </div>
+            <div className="slider">
+              <span>{parseDuration(songProgress)}</span>
+              <input type="range" min={0} value={songProgress} onChange={(event) => setSongProgress(parseInt(event.target.value))} max={songDuration} />
+              <span>-{parseDuration(songDuration - songProgress)}</span>
             </div>
           </div>
-          <div className="slider">
-            <span>{parseDuration(songProgress)}</span>
-            <input type="range" min={0} value={songProgress} onChange={(event) => setSongProgress(parseInt(event.target.value))} max={songDuration} />
-            <span>-{parseDuration(songDuration - songProgress)}</span>
+          <div className="navbar">
+            <button>
+              <RewindIcon weight="fill" />
+            </button>
+            <button onMouseDown={() => {setStatus(true); playSong();}} className={status ? "active" : ""}>
+              <PlayIcon weight="fill" />
+            </button>
+            <button onMouseDown={() => setStatus(false)} className={status ? "" : "active"}>
+              <PauseIcon weight="fill" />
+            </button>
+            <button>
+              <FastForwardIcon weight="fill" />
+            </button>
+            <button onMouseDown={() => setShowSettings(!showSettings)} className={showSettings ? "active" : ""}>
+              <FadersIcon weight="fill" />
+            </button>
           </div>
         </div>
 
-        <div className="navbar">
-          <button>
-            <RewindIcon weight="fill" />
-          </button>
-          <button onMouseDown={() => setStatus(true)} className={status ? "active" : ""}>
-            <PlayIcon weight="fill" />
-          </button>
-          <button onMouseDown={() => setStatus(false)} className={status ? "" : "active"}>
-            <PauseIcon weight="fill" />
-          </button>
-          <button>
-            <FastForwardIcon weight="fill" />
-          </button>
-          <button onMouseDown={() => setShowSettings(!showSettings)} className={showSettings ? "active" : ""}>
-            <FadersIcon weight="fill" />
-          </button>
+        <div className="footnote">
+          Powered by <SiSoundcloud />
         </div>
       </div>
+
       <noscript>
         <div className="noscript">
           <div className="noscript-box">
